ccaff8eef9b57bb82895c8016d722c355314d29a
U. I. I. Derbashi <32719218+uderbashi@users.noreply.github.com>
2020-10-28 15:52:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from cx_Freeze import setup, Executable
 
-setup(name='MS Attendance',
-	  version='0.1',
-	  description='Arrange your Attendance',
-	  executables = [Executable("MS Attendance.py")])
+setup(name='Microsoft Teams Attendance Parser',
+	  version='1.0',
+	  executables = [Executable('MSTeamsAttendance.py')])
